187bb3bd08cb1402a79cef45a6a9e16f26a8ab6d
Silmara Pizzatto <silmarapizzatto@gmail.com>
2025-02-27 11:19:24 -0400

    commit_editmsg

--- c/super_trunfo.c
+++ c/super_trunfo.c
@@ -1,6 +1,24 @@
-#include <studio.h>
 int main()
 {
-   printf("Vamos jogar Super Trunfo?\n"); 
-   scanf("%s");
+   float pib, area;
+   int populacao, turismo;
+   char estado;
+   char codigo[5];
+   char cidade[50];
+
+
+   printf("Vamos jogar Super Trunfo?\n Primeiramente vamos definir as cartas do jogo\n");
+   printf("Carta numero 1");
+
+   scanf("Digite a letra do estado      :%s", estado);
+   scanf("Digite o nome da cidade       :%s", cidade);
+   scanf("Digite a população da cidade  :%d", &populacao);
+   scanf("Digite a area total da cidade :%d", &area);
+   scanf("Digite os pontos de turismo   :%s", &turismo);
+   scanf("Digite o pib da cidade        :%s", estado);
+      
+   
+   
+   
+   
 }
